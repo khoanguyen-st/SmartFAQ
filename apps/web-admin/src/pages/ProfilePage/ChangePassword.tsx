124442419b35983c782eea64ab5f124b1b2aa722
unknown <cassiopeiachan2003@gmail.com>
2025-11-20 14:43:23 +0700

    : [SF-54] delete file error and fix layout Profile Page

--- apps/web-admin/src/pages/ProfilePage/ChangePassword.tsx
+++ apps/web-admin/src/pages/ProfilePage/ChangePassword.tsx
@@ -19,7 +19,7 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 450,
+  width: 750,
   bgcolor: 'background.paper',
   borderRadius: 2,
   boxShadow: 24,
--- apps/web-admin/src/pages/ProfilePage/ProfilePage.tsx
+++ apps/web-admin/src/pages/ProfilePage/ProfilePage.tsx
@@ -137,13 +137,13 @@ const ProfilePage = () => {
             disabled={!isEditable && !isEditing}
             onChange={handleInputChange}
             onClick={() => isEditable && handleEditClick(fieldId)}
-            className={`px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 
+            className={`px-3 py-2 border border-gray-300 rounded-md shadow-sm outline-none focus:ring-[#0003dd] focus:border-[#0003dd]
               flex-1 transition-colors duration-200 ease-in-out 
               ${
                 isEditing
-                  ? 'bg-white'
+                  ? 'bg-white focus:ring-[#003087] focus:border-[#003087] out-line-none'
                   : isEditable
-                  ? 'bg-gray-100 cursor-pointer'
+                  ? 'bg-[#f1f1f1] cursor-pointer'
                   : 'bg-gray-200 text-gray-500 cursor-not-allowed'
               }`}
           />
