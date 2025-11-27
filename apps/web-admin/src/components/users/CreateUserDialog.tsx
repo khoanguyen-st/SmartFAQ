70a3abc4d16b9fc7ba079be71908606b104b2e2b
Tri Tai <taintgcd210330@fpt.edu.vn>
2025-11-27 09:53:19 +0700

    fix(web-admin): resolve pagination bug

--- apps/web-admin/src/components/users/CreateUserDialog.tsx
+++ apps/web-admin/src/components/users/CreateUserDialog.tsx
@@ -1,7 +1,6 @@
 import React, { useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import type { CreateUserDialogProps, CreateUserDialogPayload } from '@/interfaces/create-user-dialog'
-import type { UserStatus } from '@/types/users';
 
 export const CreateUserDialog: React.FC<CreateUserDialogProps> = ({ open, onClose, onSubmit, onSuccess }) => {
   const { t } = useTranslation();
